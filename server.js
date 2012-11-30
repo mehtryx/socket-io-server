edb3cd336a25e8ada7ea7cc14a784ab38809ce5a
Keith Benedict <kbenedict@postmedia.com>
2012-11-30 08:19:26 -0500

    space/tab formatting, also adjusted heartbeat settings

--- server.js
+++ server.js
@@ -5,27 +5,29 @@ var app = require('http').createServer(handler)
 app.listen(process.env.PORT || 4455 );
 
 function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
+	fs.readFile(__dirname + '/index.html',
+	function (err, data) {
+		if (err) {
+			res.writeHead(500);
+			return res.end('Error loading index.html');
+	}
 
-    res.writeHead(200);
-    res.end(data);
-  });
+		res.writeHead(200);
+		res.end(data);
+	});
 }
 
 // assuming io is the Socket.IO server object
 io.configure(function () { 
-  io.set("transports", ["xhr-polling"]); 
-  io.set("polling duration", 10); 
+	io.set("transports", ["xhr-polling"]); 
+	io.set("polling duration", 10); 
+	io.set('heartbeat interval', 20);
+	io.set('heartbeat timeout', 60);
 });
 
 io.sockets.on('connection', function (socket) {
-  socket.on('my other event', function (data) {
-    console.log(data);
+	socket.on('my other event', function (data) {
+		console.log(data);
 	socket.broadcast.emit('message', { msg: data.msg, style: data.style, delay: data.delay });
-  });
+	});
 });
